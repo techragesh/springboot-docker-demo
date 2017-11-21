db2aae8a0dbd84663091cd73556e09feb407363b
¨ragesh.shunmugam¨ <ragesh.shunmugam@ebpi.nl>
2017-11-21 16:34:35 +0100

    Springboot Docker Demo

---
+++ springboot-docker-demo/src/test/java/com/techjava/springbootdockerdemo/SpringbootDockerDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.techjava.springbootdockerdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootDockerDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
